c71c364ec9d985f69fc24a13e2976b829f5f0bf2
Travis Campbell <hcoyote@ghostar.org>
2016-03-16 00:01:30 -0500

    Add a dryrun setting to the rsync for bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,18 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude brew.sh --exclude ".DS_Store" --exclude "bootstrap.sh" \
+	dryrun=$1
+	set -xv
+	rsync $dryrun --exclude ".git/" --exclude brew.sh --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+	set +xv
 	source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
+elif [ "$1" == "--dry-run" -o "$1" == "-n" ]; then
+	doIt -n
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
